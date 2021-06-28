1f6ca7d15a4ce4bed1c450b8cc8ba0e44960c956
MouradZ <mourad.zohri@gmail.com>
2021-06-28 17:41:58 +0200

    list events et quelques broutilles

--- Angular/src/app/routes.ts
+++ Angular/src/app/routes.ts
@@ -3,11 +3,13 @@ import { CreaEventComponent } from './crea-event/crea-event.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ListEventComponent } from './list-event/list-event.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'creationEvent', component:CreaEventComponent},
-  { path: 'inscription', component:InscriptionComponent}
+  { path: 'creationEvent', component: CreaEventComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'listEvents', component: ListEventComponent },
 ];
