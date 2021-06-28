fda29db91885cf570f60d04aeac9f3f334e568ee
Thomas <Boitelthomas@gmail.com>
2021-06-28 15:15:31 +0200

    home

--- Angular/src/app/routes.ts
+++ Angular/src/app/routes.ts
@@ -1,9 +1,10 @@
 import { CreaEventComponent } from './crea-event/crea-event.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'creationEvent', component:CreaEventComponent}
