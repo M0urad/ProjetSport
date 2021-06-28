616cef6d967e65cc6512af6fd4e9b9519433aa95
Thomas <Boitelthomas@gmail.com>
2021-06-28 14:28:38 +0200

    bootstrap et pages home + connexion

--- Angular/src/app/routes.ts
+++ Angular/src/app/routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
 ];
