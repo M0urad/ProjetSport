15476b51fbcc4957101e341e3826c3f5ef24feca
Le Nay Yann <yann.le.nay@gmail.com>
2021-06-29 14:42:31 +0200

    inscription

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CreaEventComponent } from './crea-event/crea-event.component';
 import { LoginComponent } from './login/login.component';
--- Angular/src/app/inscription/inscription.component.ts
+++ Angular/src/app/inscription/inscription.component.ts
@@ -1,4 +1,7 @@
+import { Compte } from './../model/compte';
+import { CompteService } from './../service/compte.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'inscription',
@@ -6,11 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
+  compte: Compte = new Compte();
 
-
-  constructor() { }
+  constructor(
+    private CompteService: CompteService,
+    private aR: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.aR.params.subscribe((params) => {
+      if (params.id) {
+        this.CompteService.get(params.id).subscribe((data) => {
+          this.compte = data;
+        });
+      }
+    });
   }
 
+  save() {
+    if (this.compte.id) {
+      this.CompteService.update(this.compte).subscribe((result) => {
+        this.router.navigate(['/compte']);
+      });
+    } else {
+      this.CompteService.create(this.compte).subscribe((result) => {
+        this.router.navigate(['/compte']);
+      });
+    }
+  }
 }
