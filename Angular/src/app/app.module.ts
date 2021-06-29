38faac79b18b557776f5b2c8736674b63d5e535a
Thomas <Boitelthomas@gmail.com>
2021-06-29 16:57:03 +0200

    formcontrol pour connexion

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -24,6 +24,7 @@ import { InscriptionComponent } from './inscription/inscription.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
   ],
   providers: [],
