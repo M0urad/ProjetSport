ced6cde1585a5243719030465db12137f3b010c5
Frédéric <f.blosse@wanadoo.fr>
2021-06-30 09:59:49 +0200

    authservice config security

--- ProjetFinalJava/src/main/java/application/repositories/CompteRepository.java
+++ ProjetFinalJava/src/main/java/application/repositories/CompteRepository.java
@@ -11,4 +11,7 @@ import application.entities.Compte;
 public interface CompteRepository extends JpaRepository<Compte, Integer> {
 	@Query("select id from Compte where id=:id")
 	Optional<Compte> findById(@Param("id") Integer id);
+	
+	Optional<Compte> findByUsername(String username);
+	
 }
